6c8c744d355b5ad2bbf7a4d75c99cc2994488c5d
keecpe10 <40386831+keecpe10@users.noreply.github.com>
2022-07-04 16:40:55 +0700

    เพิ่มบล็อกตั้งค่าเซ็นเซอร์ เปลี่ยนการคำนวณหาเส้น

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 LR2Sensor.InitStart()
+LR2Sensor.SetValueSensors(SensorsNumber.L, 0, 0)
+LR2Sensor.SetValueSensors(SensorsNumber.R, 0, 0)
 LR2Sensor.ButtonWaite(LineRobotButton.A)
-basic.showIcon(IconNames.Tortoise)
-LR2Sensor.SaveValueSensors()
-LR2Sensor.ButtonWaite(LineRobotButton.B)
 basic.showIcon(IconNames.Butterfly)
 basic.forever(function () {
     LR2Sensor.FollowerLine(500, 0.09, 2.3)
